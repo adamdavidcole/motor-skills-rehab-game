5dd591fe9d4d0cda5f08b8ec759f91f5d2482e57
adamcolewix <adamcole@seas.upenn.edu>
2015-04-01 20:10:31 -0400

    game library and framework

---
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package com.mygdx.game.android;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+        Log.d("tag", "do we create this?");
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = false;
+        config.useCompass = false;
+        initialize(new MyGdxGame(), config);
+	}
+}
