ec9f8e61c12e175688ca304b038c184f86852e1e
William Schiela <bschiela@gmail.com>
2015-04-15 10:12:47 -0400

    coins spawn in sinusoidal path with randomly varying amplitude that changes every half-period

--- core/src/com/mygdx/game/CoinPath.java
+++ core/src/com/mygdx/game/CoinPath.java
@@ -27,6 +27,14 @@ public class CoinPath {
     private int width;
     private int height;
 
+    // sinusoidal path paramters
+    private double PI = Math.PI;
+    private int amplitude;
+    private boolean inSecondHalfPeriod;
+    private double period;
+    private double t0;
+
+
     public CoinPath(int width, int height) {
         this.width = width;
         this.height = height;
@@ -34,13 +42,18 @@ public class CoinPath {
         coinImage = new Texture(Gdx.files.internal("coin.png"));
         //coinSound = Gdx.audio.newSound(Gdx.files.internal("coin.wav"));
 
+        amplitude = randomAmplitude();
+        period = 4000000000.; // nanoseconds
+        t0 = (double) TimeUtils.nanoTime();
+        inSecondHalfPeriod = false;
+
         // spawn the first coin
         spawnCoin();
 
     }
 
     public void updateCoinPath(Rectangle character) {
-        if (TimeUtils.nanoTime() - lastCoinTime > 1000000000) {
+        if (TimeUtils.nanoTime() - lastCoinTime > 500000000) {
             spawnCoin();
         }
         // move the coins, remove any that hit the character or are above the edge of the screen
@@ -57,13 +70,34 @@ public class CoinPath {
                 iter.remove();
             }
         }
+    }
 
+    private int randomAmplitude() {
+        return MathUtils.random(0,(width-64)/2);
+    }
+
+    private float computeOptimalPath() {
+        double t = (double) TimeUtils.nanoTime() - t0;
+        // randomize amplitude every half-period
+        if (t > period/2 && !inSecondHalfPeriod) {
+            inSecondHalfPeriod = true;
+            amplitude = randomAmplitude();
+        } else if (t > period) {
+            inSecondHalfPeriod = false;
+            amplitude = randomAmplitude();
+            t0 = (double) TimeUtils.nanoTime();
+        }
+        double offset = (width - 64) / 2;
+        double argument = 2*PI*t/period;
+
+        float xPosition = (float) (offset + amplitude*Math.sin(argument));
+        return xPosition;
     }
 
     private void spawnCoin() {
         Rectangle coin = new Rectangle();
-        coin.x = MathUtils.random(0, width - 64);
-        coin.y = 0;
+        coin.x = computeOptimalPath();
+        coin.y = -64;
         coin.width = 64;
         coin.height = 64;
         coins.add(coin);
